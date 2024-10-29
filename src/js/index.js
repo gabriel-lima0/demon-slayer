58e83101f5c191ab4da01894ecb7dc1c20b44738
Gabriel-Lima0 <gabbriel.llima7@gmail.com>
2024-10-28 22:51:14 -0300

    functions js

--- src/js/index.js
+++ src/js/index.js
@@ -10,24 +10,51 @@ listaSelecaoPersonagens.forEach((personagem) => {
     personagem.addEventListener("click", () => {
         
         // PASSO 3 - remover a classe aberto só do cartão que tá aberto
-        const cartaoPersonagemAberto = document.querySelector(".aberto");
-        cartaoPersonagemAberto.classList.remove("aberto");
+        removerClassAberto();
 
         // PASSO 4 - ao clicar em um personagem da lista pegamos o id desse personagem para saber qual cartão abrir
-        const idPersonagemSelecionado = personagem.attributes.id.value;
-        const idCartaoPersonagemParaAbrir = "cartao-" + idPersonagemSelecionado;
-        const cartaoPersonagemParaAbrir = document.getElementById(idCartaoPersonagemParaAbrir);
+        const { cartaoPersonagemParaAbrir, idPersonagemSelecionado } = idDoPersonagemSelecionadoDaLista(personagem);
 
         cartaoPersonagemParaAbrir.classList.add("aberto");
         // return idPersonagemSelecionado;
 
-        // PASSO 5 - remover a classe ativo que marca o pokedev selecionado na listagem
-        const personagemAtivoNaListagem = document.querySelector(".ativo");
-        personagemAtivoNaListagem.classList.remove("ativo");
+        // PASSO 5 - remover a classe ativo que marca o personagem selecionado na listagem
+        removerClassAtivo();
 
-        // PASSO 6 - adicinar a classe ativo no pokedev selecionado na listagem
-        const personagemSelecionadoNaListagem = document.getElementById(idPersonagemSelecionado);
-        personagemSelecionadoNaListagem.classList.add("ativo");
+        // PASSO 6 - adicinar a classe ativo no personagem selecionado na listagem
+        adicionarClassAtivoPersonagemSelecionado(idPersonagemSelecionado);
 
     });
 });
+
+            // FUNCTIONS
+
+ // PASSO 3 - remover a classe aberto só do cartão que tá aberto
+function removerClassAberto() {
+    const cartaoPersonagemAberto = document.querySelector(".aberto");
+    cartaoPersonagemAberto.classList.remove("aberto");
+}
+
+// PASSO 4 - ao clicar em um personagem da lista pegamos o id desse personagem para saber qual cartão abrir
+function idDoPersonagemSelecionadoDaLista(personagem) {
+    const idPersonagemSelecionado = personagem.attributes.id.value;
+    const idCartaoPersonagemParaAbrir = "cartao-" + idPersonagemSelecionado;
+    const cartaoPersonagemParaAbrir = document.getElementById(
+        idCartaoPersonagemParaAbrir
+    );
+    return { cartaoPersonagemParaAbrir, idPersonagemSelecionado };
+}
+
+// PASSO 5 - remover a classe ativo que marca o personagem selecionado na listagem
+function removerClassAtivo() {
+    const personagemAtivoNaListagem = document.querySelector(".ativo");
+    personagemAtivoNaListagem.classList.remove("ativo");
+}
+
+// PASSO 6 - adicinar a classe ativo no personagem selecionado na listagem
+function adicionarClassAtivoPersonagemSelecionado(idPersonagemSelecionado) {
+    const personagemSelecionadoNaListagem = document.getElementById(
+        idPersonagemSelecionado
+    );
+    personagemSelecionadoNaListagem.classList.add("ativo");
+}
